46829145be17e8f533462bdd3e4566115cdda37d
shen173869710 <173869710@qq.com>
2021-01-14 16:31:25 +0800

    提交状态不同步Bug

--- app/src/main/java/com/auto/di/guan/rtm/MessageSend.java
+++ app/src/main/java/com/auto/di/guan/rtm/MessageSend.java
@@ -83,7 +83,7 @@ public class MessageSend {
     public static void syncAutoStatus() {
         MessageInfo info = new MessageInfo();
         info.setType(MessageEntiy.TYPE_AUTO_STATUS);
-        info.setControlInfos(ControlInfoSql.queryControlList());
+        info.setDeviceInfos(DeviceInfoSql.queryDeviceList());
         info.setGroupInfos(GroupInfoSql.queryGroupList());
         send(info);
     }
@@ -94,7 +94,7 @@ public class MessageSend {
     public static void syncAutoClose() {
         MessageInfo info = new MessageInfo();
         info.setType(MessageEntiy.TYPE_AUTO_CLOSE);
-        info.setControlInfos(ControlInfoSql.queryControlList());
+        info.setDeviceInfos(DeviceInfoSql.queryDeviceList());
         info.setGroupInfos(GroupInfoSql.queryGroupList());
         send(info);
     }
